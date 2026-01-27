40f2e7c7b917e7c476ced9ea05285afcee1d2ec1
SintaraAMV <hikado95@gmail.com>
2026-01-27 14:52:49 -0500

    Refactor Maze class to improve movement logic and add direction validation

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,69 +1,55 @@
-/// <summary>
-/// Defines a maze using a dictionary. The dictionary is provided by the
-/// user when the Maze object is created. The dictionary will contain the
-/// following mapping:
-///
-/// (x,y) : [left, right, up, down]
-///
-/// 'x' and 'y' are integers and represents locations in the maze.
-/// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
-///
-/// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
-///
-/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
-/// then the 'currX' and 'currY' values should be changed.
-/// </summary>
-public class Maze
+using System;
+using System.Collections.Generic;
+
+namespace Mase
 {
-    private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
-    private int _currX = 1;
-    private int _currY = 1;
-
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+    public class Maze
     {
-        _mazeMap = mazeMap;
-    }
-
-    // TODO Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveLeft()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveRight()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveUp()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveDown()
-    {
-        // FILL IN CODE
-    }
-
-    public string GetStatus()
-    {
-        return $"Current location (x={_currX}, y={_currY})";
+        private Dictionary<(int, int), bool[]> _map;
+        private (int x, int y) _currentPosition;
+        
+        public Maze(Dictionary<(int, int), bool[]> map)
+        {
+            _map = map;
+            _currentPosition = (1, 1);
+        }
+        
+        public string GetStatus()
+        {
+            return $"Current location (x={_currentPosition.x}, y={_currentPosition.y})";
+        }
+        
+        public void MoveUp()
+        {
+            var (x, y) = _currentPosition;
+            // Asumiendo que bool[] contiene [left, right, up, down]
+            if (!_map.ContainsKey((x, y)) || !_map[(x, y)][2])
+                throw new InvalidOperationException();
+            _currentPosition = (x, y - 1);
+        }
+        
+        public void MoveDown()
+        {
+            var (x, y) = _currentPosition;
+            if (!_map.ContainsKey((x, y)) || !_map[(x, y)][3])
+                throw new InvalidOperationException();
+            _currentPosition = (x, y + 1);
+        }
+        
+        public void MoveLeft()
+        {
+            var (x, y) = _currentPosition;
+            if (!_map.ContainsKey((x, y)) || !_map[(x, y)][0])
+                throw new InvalidOperationException();
+            _currentPosition = (x - 1, y);
+        }
+        
+        public void MoveRight()
+        {
+            var (x, y) = _currentPosition;
+            if (!_map.ContainsKey((x, y)) || !_map[(x, y)][1])
+                throw new InvalidOperationException();
+            _currentPosition = (x + 1, y);
+        }
     }
 }
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,106 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 public static class SetsAndMaps
 {
-    /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for returning all symmetric pairs of words.  
-    ///
-    /// For example, if words was: [am, at, ma, if, fi], we would return :
-    ///
-    /// ["am & ma", "if & fi"]
-    ///
-    /// The order of the array does not matter, nor does the order of the specific words in each string in the array.
-    /// at would not be returned because ta is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be returned.
-    /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    public static string[] FindPairs(string[] words)
+   // Problem 1: Find Pairs
+public static string[] FindPairs(string[] words)
+{
+    var seen = new HashSet<string>();
+    var pairs = new List<string>();
+
+    foreach (var word in words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        // El enunciado asume palabras de 2 caracteres; esto evita reventar si llega algo raro.
+        if (string.IsNullOrEmpty(word) || word.Length != 2)
+            continue;
+
+        // Skip palindromes (same character words), ej: "aa"
+        if (word[0] == word[1])
+            continue;
+
+        string reversed = $"{word[1]}{word[0]}";
+
+        if (seen.Contains(reversed))
+        {
+            //  "ma & am", "fi & if" (el que “cierra” el par va primero)
+            pairs.Add($"{word} & {reversed}");
+        }
+        else
+        {
+            seen.Add(word);
+        }
     }
 
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
+    return pairs.ToArray();
+}
+
+
+    // Problem 2: Degree Summary
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
-        var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename))
+        var degreeCounts = new Dictionary<string, int>();
+
+        try
         {
-            var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var lines = File.ReadAllLines(filename);
+
+            foreach (var line in lines)
+            {
+                var columns = line.Split(',');
+
+                // Ensure we have at least 4 columns
+                if (columns.Length >= 4)
+                {
+                    var degree = columns[3].Trim();
+
+                    if (!string.IsNullOrEmpty(degree))
+                    {
+                        if (degreeCounts.ContainsKey(degree))
+                            degreeCounts[degree]++;
+                        else
+                            degreeCounts[degree] = 1;
+                    }
+                }
+            }
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
         }
 
-        return degrees;
+        return degreeCounts;
     }
 
-    /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
-    /// </summary>
+    // Problem 3: Anagrams
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        // Clean strings: remove spaces and convert to lowercase
+        string clean1 = word1.Replace(" ", "").ToLower();
+        string clean2 = word2.Replace(" ", "").ToLower();
+
+        // Quick length check
+        if (clean1.Length != clean2.Length)
+            return false;
+
+        // Count letters in first word
+        var letterCounts = new Dictionary<char, int>();
+        foreach (char c in clean1)
+        {
+            if (letterCounts.ContainsKey(c))
+                letterCounts[c]++;
+            else
+                letterCounts[c] = 1;
+        }
+
+        // Compare with second word
+        foreach (char c in clean2)
+        {
+            if (!letterCounts.ContainsKey(c))
+                return false;
+
+            letterCounts[c]--;
+            if (letterCounts[c] == 0)
+                letterCounts.Remove(c);
+        }
+
+        return letterCounts.Count == 0;
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will return a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
-    public static string[] EarthquakeDailySummary()
+    // Problem 5: Earthquake JSON Data
+    public static async Task<string[]> EarthquakeDailySummary()
     {
-        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
         using var client = new HttpClient();
-        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-        using var reader = new StreamReader(jsonStream);
-        var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+        try
+        {
+            // USGS API for daily earthquakes
+            string url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+            string json = await client.GetStringAsync(url);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+            // Deserialize JSON
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var data = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+            // Format output
+            var results = new List<string>();
+            foreach (var feature in data.Features)
+            {
+                string place = feature.Properties.Place;
+                double mag = feature.Properties.Mag;
+                results.Add($"{place} - Mag {mag:F2}");
+            }
+
+            return results.ToArray();
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error fetching earthquake data: {ex.Message}");
+            return new string[0];
+        }
     }
 }
