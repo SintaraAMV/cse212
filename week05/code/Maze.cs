2c8686aacb24cc4788af1eaeb5b5e8ba8ab7482b
Omar Lizandro <hikado95@gmail.com>
2026-06-10 16:29:08 -0500

    Remove obsolete files and refactor Maze and Recursion classes for improved functionality

--- week05/code/Maze.cs
+++ week05/code/Maze.cs
@@ -1,82 +1,54 @@
 // DO NOT MODIFY THIS FILE
 
-using System;
-using System.Collections.Generic;
-
-namespace Recursion
+public class Maze
 {
-    public static class Maze
+    public int Width { get; }
+    public int Height { get; }
+    public readonly int[] Data;
+
+    public Maze(int width, int height, int[] data)
     {
-        // DO NOT MODIFY THIS FILE - the helpers are already written for you
-
-        public static void SolveMaze(int[] maze, int size, List<string> results)
-        {
-            List<(int x, int y)> currPath = new List<(int x, int y)>();
-            SolveMazeHelper(maze, size, 0, 0, currPath, results);
-        }
-
-        private static void SolveMazeHelper(int[] maze, int size, int x, int y,
-            List<(int x, int y)> currPath, List<string> results)
-        {
-            // Add current position to path
-            currPath.Add((x, y));
-
-            if (IsEnd(maze, size, x, y))
-            {
-                results.Add(currPath.AsString());
-            }
-            else
-            {
-                // Try moving right
-                if (IsValidMove(maze, size, x + 1, y, currPath))
-                    SolveMazeHelper(maze, size, x + 1, y, currPath, results);
-
-                // Try moving down
-                if (IsValidMove(maze, size, x, y + 1, currPath))
-                    SolveMazeHelper(maze, size, x, y + 1, currPath, results);
-
-                // Try moving left
-                if (IsValidMove(maze, size, x - 1, y, currPath))
-                    SolveMazeHelper(maze, size, x - 1, y, currPath, results);
-
-                // Try moving up
-                if (IsValidMove(maze, size, x, y - 1, currPath))
-                    SolveMazeHelper(maze, size, x, y - 1, currPath, results);
-            }
-
-            // Backtrack
-            currPath.RemoveAt(currPath.Count - 1);
-        }
-
-        // The helper methods below are already provided by the course
-        private static bool IsEnd(int[] maze, int size, int x, int y)
-        {
-            return maze[y * size + x] == 2;
-        }
-
-        private static bool IsValidMove(int[] maze, int size, int x, int y, List<(int x, int y)> currPath)
-        {
-            if (x < 0 || x >= size || y < 0 || y >= size)
-                return false;
-
-            if (maze[y * size + x] == 0)
-                return false;
-
-            foreach (var pos in currPath)
-            {
-                if (pos.x == x && pos.y == y)
-                    return false;
-            }
-            return true;
-        }
+        Width = width;
+        Height = height;
+        Data = data;
     }
 
-    // Extension method used by the tests
-    public static class ListExtensions
+    /// <summary>
+    /// Helper function to determine if the (x,y) position is at
+    /// the end of the maze.
+    /// </summary>
+    public bool IsEnd(int x, int y)
     {
-        public static string AsString(this List<(int x, int y)> list)
+        return Data[y * Width + x] == 2;
+    }
+
+    /// <summary>
+    /// Helper function to determine if the (x,y) position is a valid
+    /// place to move given the size of the maze, the content of the maze,
+    /// and the current path already traversed.
+    /// </summary>
+    public bool IsValidMove(List<ValueTuple<int, int>> currPath, int x, int y)
+    {
+        if (x > Width - 1 || x < 0)
         {
-            return string.Join(" -> ", list.Select(p => $"({p.x},{p.y})"));
+            return false;
         }
+
+        if (y > Height - 1 || y < 0)
+        {
+            return false;
+        }
+
+        if (Data[y * Width + x] == 0)
+        {
+            return false;
+        }
+
+        if (currPath.Contains((x, y)))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,90 +1,148 @@
-using System;
 using System.Collections.Generic;
 
-namespace Recursion
+public static class Recursion
 {
-    public static class Recursion
+    /// <summary>
+    /// #############
+    /// # Problem 1 #
+    /// #############
+    /// Using recursion, find the sum of 1^2 + 2^2 + 3^2 + ... + n^2.
+    /// If n <= 0, return 0. A loop should not be used.
+    /// </summary>
+    public static int SumSquaresRecursive(int n)
     {
-        /// #############
-        /// # Problem 1 #
-        /// #############
-        public static int SumSquaresRecursive(int n)
+        if (n <= 0)
         {
-            if (n <= 0)
-                return 0;
-            return (n * n) + SumSquaresRecursive(n - 1);
+            return 0;
         }
 
-        /// #############
-        /// # Problem 2 #
-        /// #############
-        public static void PermutationsChoose(char[] letters, int size, List<string> results)
+        return (n * n) + SumSquaresRecursive(n - 1);
+    }
+
+    /// <summary>
+    /// #############
+    /// # Problem 2 #
+    /// #############
+    /// Using recursion, insert permutations of length 'size'
+    /// from the string 'letters' into the results list.
+    /// </summary>
+    public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
+    {
+        if (word.Length == size)
         {
-            PermutationsChooseHelper(letters, size, "", results);
+            results.Add(word);
+            return;
         }
 
-        private static void PermutationsChooseHelper(char[] letters, int size, string current, List<string> results)
+        for (int i = 0; i < letters.Length; i++)
         {
-            if (current.Length == size)
+            string remainingLetters = letters.Remove(i, 1);
+            PermutationsChoose(results, remainingLetters, size, word + letters[i]);
+        }
+    }
+
+    /// <summary>
+    /// #############
+    /// # Problem 3 #
+    /// #############
+    /// Count how many ways there are to climb stairs when a person
+    /// can climb 1, 2, or 3 stairs at a time.
+    /// Uses memoization for large values.
+    /// </summary>
+    public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
+    {
+        remember ??= new Dictionary<int, decimal>();
+
+        if (s < 0)
+        {
+            return 0;
+        }
+
+        if (s == 0)
+        {
+            return 1;
+        }
+
+        if (remember.ContainsKey(s))
+        {
+            return remember[s];
+        }
+
+        decimal result =
+            CountWaysToClimb(s - 1, remember) +
+            CountWaysToClimb(s - 2, remember) +
+            CountWaysToClimb(s - 3, remember);
+
+        remember[s] = result;
+        return result;
+    }
+
+    /// <summary>
+    /// #############
+    /// # Problem 4 #
+    /// #############
+    /// Insert all possible binary strings for a given wildcard pattern.
+    /// Example: 1*0 -> 100, 110.
+    /// </summary>
+    public static void WildcardBinary(string pattern, List<string> results)
+    {
+        int index = pattern.IndexOf('*');
+
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        WildcardBinary(pattern[..index] + "0" + pattern[(index + 1)..], results);
+        WildcardBinary(pattern[..index] + "1" + pattern[(index + 1)..], results);
+    }
+
+    /// <summary>
+    /// #############
+    /// # Problem 5 #
+    /// #############
+    /// Use recursion to insert all paths that start at (0,0)
+    /// and end at the end square into the results list.
+    /// </summary>
+    public static void SolveMaze(
+        List<string> results,
+        Maze maze,
+        int x = 0,
+        int y = 0,
+        List<ValueTuple<int, int>>? currPath = null)
+    {
+        currPath ??= new List<ValueTuple<int, int>>();
+
+        currPath.Add((x, y));
+
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+        }
+        else
+        {
+            if (maze.IsValidMove(currPath, x + 1, y))
             {
-                results.Add(current);
-                return;
+                SolveMaze(results, maze, x + 1, y, currPath);
             }
 
-            for (int i = 0; i < letters.Length; i++)
+            if (maze.IsValidMove(currPath, x, y + 1))
             {
-                PermutationsChooseHelper(letters, size, current + letters[i], results);
+                SolveMaze(results, maze, x, y + 1, currPath);
+            }
+
+            if (maze.IsValidMove(currPath, x - 1, y))
+            {
+                SolveMaze(results, maze, x - 1, y, currPath);
+            }
+
+            if (maze.IsValidMove(currPath, x, y - 1))
+            {
+                SolveMaze(results, maze, x, y - 1, currPath);
             }
         }
 
-        /// #############
-        /// # Problem 3 #
-        /// #############
-        public static int CountWaysToClimb(int s, Dictionary<int, int> remember = null)
-        {
-            if (remember == null)
-                remember = new Dictionary<int, int>();
-
-            if (s < 0) return 0;
-            if (s == 0) return 1;
-
-            if (remember.ContainsKey(s))
-                return remember[s];
-
-            int result = CountWaysToClimb(s - 1, remember) +
-                         CountWaysToClimb(s - 2, remember) +
-                         CountWaysToClimb(s - 3, remember);
-
-            remember[s] = result;
-            return result;
-        }
-
-        /// #############
-        /// # Problem 4 #
-        /// #############
-        public static void WildcardBinary(string pattern, List<string> results)
-        {
-            WildcardBinaryHelper(pattern, "", results);
-        }
-
-        private static void WildcardBinaryHelper(string pattern, string current, List<string> results)
-        {
-            if (current.Length == pattern.Length)
-            {
-                results.Add(current);
-                return;
-            }
-
-            int index = current.Length;
-            if (pattern[index] == '*')
-            {
-                WildcardBinaryHelper(pattern, current + "0", results);
-                WildcardBinaryHelper(pattern, current + "1", results);
-            }
-            else
-            {
-                WildcardBinaryHelper(pattern, current + pattern[index], results);
-            }
-        }
+        currPath.RemoveAt(currPath.Count - 1);
     }
 }
